5b9c820b6eab9ceac70371b0a58c6cd556278ed5
Jochen Szostek <jochen@jochen-ThinkPad-W510.(none)>
2013-01-08 15:24:39 +0100

    init

---
+++ filtered-app.js
@@ -0,0 +1,11 @@
+require('./lib/prefabLOG.js')('filtered');
+// require('./lib/prefabLOG.js')({logger: null});
+
+// console.log(console);
+
+console.log('log test %s %d %j', 'string', 123, {foo:'bar'});
+console.info('info test %s %d %j', 'string', 123, {foo:'bar'});
+console.warn('warn test %s %d %j', 'string', 123, {foo:'bar'});
+console.error('error test %s %d %j', 'string', 123, {foo:'bar'});
+// console.trace('trace label');
+
